b459035182c8206ca1dc8fa12329ae034fbeb8d7
sergio-guzman-xk <57773735+sergio-guzman-xk@users.noreply.github.com>
2020-11-08 21:27:48 -0500

    Created a Fake Main class in order to build a .jar artifact. Since JDK 11 can't build javafx artifacts the fake class was build to create the .jar artifact. Added a few more detailed errors.

--- src/mainFake.java
+++ src/mainFake.java
@@ -1,7 +1,7 @@
-import java.sql.SQLException;
-
-public class mainFake {
-    public static void main(String[] args) throws SQLException {
-        sample.Main.main(args);
-    }
-}
+//import java.sql.SQLException;
+//
+//public class mainFake {
+//    public static void main(String[] args) throws SQLException {
+//        sample.Main.main(args);
+//    }
+//}
